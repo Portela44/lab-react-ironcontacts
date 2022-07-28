b8d52a3882f823d8721c4128bdb283aa85436ad2
Guillem <guillem.mart7@gmail.com>
2022-07-28 12:07:55 +0200

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, {useState} from "react"
 import './App.css';
+import contacts from "./contacts.json";
+
 
 function App() {
+  const {contactArr, setContacts} = useState([contacts[0], contacts[1], contacts[2], contacts[3], contacts[4]]); 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <table className="contactTable">
+        <tbody>
+          <tr className="tableHeaders">
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+          <tr className="tableContact">
+            <th><img src={contacts[0].pictureUrl} alt={contacts[0].name}></img></th>
+            <th>{contacts[0].name}</th>
+            <th>{contacts[0].popularity.toFixed(2)}</th>
+          </tr>
+          <tr className="tableContact">
+            <th><img src={contacts[1].pictureUrl} alt={contacts[1].name}></img></th>
+            <th>{contacts[1].name}</th>
+            <th>{contacts[1].popularity.toFixed(2)}</th>
+          </tr>
+          <tr className="tableContact">
+            <th><img src={contacts[2].pictureUrl} alt={contacts[2].name}></img></th>
+            <th>{contacts[2].name}</th>
+            <th>{contacts[2].popularity.toFixed(2)}</th>
+          </tr>
+          <tr className="tableContact">
+            <th><img src={contacts[3].pictureUrl} alt={contacts[3].name}></img></th>
+            <th>{contacts[3].name}</th>
+            <th>{contacts[3].popularity.toFixed(2)}</th>
+          </tr>
+          <tr className="tableContact">
+            <th><img src={contacts[4].pictureUrl} alt={contacts[4].name}></img></th>
+            <th>{contacts[4].name}</th>
+            <th>{(contacts[4].popularity).toFixed(2)}</th>
+          </tr>
+        </tbody>
+      </table>       
     </div>
   );
 }
