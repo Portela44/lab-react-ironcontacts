32a9445ca123dab5df9a85197565db526f035f65
Guillem <guillem.mart7@gmail.com>
2022-07-28 12:27:46 +0200

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -15,31 +15,43 @@ function App() {
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
           </tr>
           <tr className="tableContact">
             <th><img src={contacts[0].pictureUrl} alt={contacts[0].name}></img></th>
             <th>{contacts[0].name}</th>
             <th>{contacts[0].popularity.toFixed(2)}</th>
+            {contacts[0].wonOscar && <th>🏆</th>}
+            {contacts[0].wonEmmy && <th>🏆</th>}
           </tr>
           <tr className="tableContact">
             <th><img src={contacts[1].pictureUrl} alt={contacts[1].name}></img></th>
             <th>{contacts[1].name}</th>
             <th>{contacts[1].popularity.toFixed(2)}</th>
+            {contacts[1].wonOscar && <th>🏆</th>}
+            {contacts[1].wonEmmy && <th>🏆</th>}
           </tr>
           <tr className="tableContact">
             <th><img src={contacts[2].pictureUrl} alt={contacts[2].name}></img></th>
             <th>{contacts[2].name}</th>
             <th>{contacts[2].popularity.toFixed(2)}</th>
+            {contacts[2].wonOscar && <th>🏆</th>}
+            {contacts[2].wonEmmy && <th>🏆</th>}
           </tr>
           <tr className="tableContact">
             <th><img src={contacts[3].pictureUrl} alt={contacts[3].name}></img></th>
             <th>{contacts[3].name}</th>
             <th>{contacts[3].popularity.toFixed(2)}</th>
+            {contacts[3].wonOscar && <th>🏆</th>}
+            {contacts[3].wonEmmy && <th>🏆</th>}
           </tr>
           <tr className="tableContact">
             <th><img src={contacts[4].pictureUrl} alt={contacts[4].name}></img></th>
             <th>{contacts[4].name}</th>
             <th>{(contacts[4].popularity).toFixed(2)}</th>
+            {contacts[4].wonOscar && <th>🏆</th>}
+            {contacts[4].wonEmmy && <th>🏆</th>}
           </tr>
         </tbody>
       </table>       
