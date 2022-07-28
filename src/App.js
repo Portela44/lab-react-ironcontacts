698b157cb0427bacd587354cbc9331975597f807
Guillem <guillem.mart7@gmail.com>
2022-07-28 19:36:04 +0200

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import './App.css';
-import contacts from "./contacts.json";
-
+import contactData from "./contacts.json";
 
 function App() {
-  const {contactArr, setContacts} = useState([contacts[0], contacts[1], contacts[2], contacts[3], contacts[4]]); 
-  
+  const [showContacts, setShowContacts] = useState([...contactData].splice(0,5));
+  const [remainingContacts, setRemainingContacts] = useState([...contactData].splice(5));
+  function addRandomContact(showContacts, remainingContacts) {
+    const random = Math.round(Math.random()*remainingContacts.length);
+    showContacts.push(remainingContacts[random]);
+    setShowContacts(showContacts);
+    remainingContacts = remainingContacts.filter(contact => contact.name !== remainingContacts[random].name);
+    setRemainingContacts(remainingContacts);
+  };
   return (
     <div className="App">
       <h1>IronContacts</h1>
+      <button className="random-btn" onClick={() => addRandomContact(showContacts, remainingContacts)}>Add Random Contact</button>
       <table className="contactTable">
         <tbody>
           <tr className="tableHeaders">
@@ -18,45 +25,20 @@ function App() {
             <th>Won Oscar</th>
             <th>Won Emmy</th>
           </tr>
-          <tr className="tableContact">
-            <th><img src={contacts[0].pictureUrl} alt={contacts[0].name}></img></th>
-            <th>{contacts[0].name}</th>
-            <th>{contacts[0].popularity.toFixed(2)}</th>
-            {contacts[0].wonOscar && <th>🏆</th>}
-            {contacts[0].wonEmmy && <th>🏆</th>}
-          </tr>
-          <tr className="tableContact">
-            <th><img src={contacts[1].pictureUrl} alt={contacts[1].name}></img></th>
-            <th>{contacts[1].name}</th>
-            <th>{contacts[1].popularity.toFixed(2)}</th>
-            {contacts[1].wonOscar && <th>🏆</th>}
-            {contacts[1].wonEmmy && <th>🏆</th>}
-          </tr>
-          <tr className="tableContact">
-            <th><img src={contacts[2].pictureUrl} alt={contacts[2].name}></img></th>
-            <th>{contacts[2].name}</th>
-            <th>{contacts[2].popularity.toFixed(2)}</th>
-            {contacts[2].wonOscar && <th>🏆</th>}
-            {contacts[2].wonEmmy && <th>🏆</th>}
-          </tr>
-          <tr className="tableContact">
-            <th><img src={contacts[3].pictureUrl} alt={contacts[3].name}></img></th>
-            <th>{contacts[3].name}</th>
-            <th>{contacts[3].popularity.toFixed(2)}</th>
-            {contacts[3].wonOscar && <th>🏆</th>}
-            {contacts[3].wonEmmy && <th>🏆</th>}
-          </tr>
-          <tr className="tableContact">
-            <th><img src={contacts[4].pictureUrl} alt={contacts[4].name}></img></th>
-            <th>{contacts[4].name}</th>
-            <th>{(contacts[4].popularity).toFixed(2)}</th>
-            {contacts[4].wonOscar && <th>🏆</th>}
-            {contacts[4].wonEmmy && <th>🏆</th>}
-          </tr>
+          {showContacts.map(contact => {
+            return (
+              <tr className="tableContact" key={contact.name}>
+                <th><img src={contact.pictureUrl} alt={contact.name}></img></th>
+                <th>{contact.name}</th>
+                <th>{contact.popularity.toFixed(2)}</th>
+                {contact.wonOscar && <th>🏆</th>}
+                {contact.wonEmmy && <th>🏆</th>}
+              </tr>
+            );
+          })}
         </tbody>
-      </table>       
+      </table>
     </div>
   );
-}
-
+};
 export default App;
